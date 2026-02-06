d14412a8d36305da40e435e1a0b4da84bf9db5a0
Aruzhan <dniyararuzhan@gmail.com>
2026-02-06 20:35:03 +0500

    Add main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+def analyze_text(text):
+    vowels="aejouy"
+    unique_vowels=set()
+    clean=" "
+    for ch in text:
+        if ch.isalpha():
+            clean +=ch.lower
+        else:
+            clean+=" "
+    words=clean.split()
+    result=[]
+    used=set()
+    for w in words:
+        for ch in vowels:
+            if ch in vowels:
+                unique_vowels.add(ch)
+        if len(w)>=5 and w[0]==w[-1]:
+            if w not in used:
+                result.append(w)
+                used.add(w)
+        return len(unique_vowels)," ".join(result)
